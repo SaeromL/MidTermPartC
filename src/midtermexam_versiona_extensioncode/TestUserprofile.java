428dbd26429ef79198866f48855d5662b4417c34
SaeromL <leesae@sheridancollege.ca>
2022-02-23 10:09:24 -0500

    Created TestUserprofile.java

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Saerom Lee
+ */
+public class TestUserprofile {
+    
+    public static void main(String[] args) {
+        
+        UserProfile user1 = new UserProfile("SAEROM", "Comedy");
+        System.out.println("New user profile ID " + user1.getUserID() + " was created");
+        
+    }
+    
+}
